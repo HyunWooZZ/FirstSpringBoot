c40a24a148dbe814df2946f9fca6417625c5fbba
HyunWoo Oh <gusdn959@gmail.com>
2024-05-07 14:45:42 +0900

    SpringBoot logging practice

--- src/main/kotlin/com/hyunwoo/firstboot/HelloController.kt
+++ src/main/kotlin/com/hyunwoo/firstboot/HelloController.kt
@@ -4,17 +4,32 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
+import org.slf4j.LoggerFactory
 
 data class Message(
     val message: String,
     val buildNumber: String
 )
-
 @RestController
-class GreetingController(
+class HelloController(
     @Value("\${hyunwoo.firstboot.buildNumber}") val buildNumber: String
 ) {
+    val logger = LoggerFactory.getLogger(this::class.java)
     @GetMapping("/hello")
-    fun hello(): Message = Message("Hello JY :)", buildNumber)
+    fun hello(): Message {
+        logger.trace("trace the line")
+        logger.debug("debug the line")
+
+        logger.info("info the line")
+        logger.warn("warn the line")
+        logger.error("error the Line")
+
+        try {
+            val x: Int = 1 / 0
+        } catch (e: ArithmeticException) {
+            logger.error("Error: failed to compute the x", e)
+        }
+
+        return Message("Hello JY :)", buildNumber)
+    }
 }
